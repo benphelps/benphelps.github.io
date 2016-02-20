bb0e11dc5319461d87697c213a1bcca508e6a327
Ben Phelps <ben@phelps.io>
2016-02-19 23:54:40 -0600

    Cleanup

--- ipcam/js/gifworker.js
+++ ipcam/js/gifworker.js
@@ -1,26 +1,16 @@
 importScripts("whammy.js");
-importScripts("b64.js");
 
 self.addEventListener('message', function(e) {
   var data = e.data[0].reverse();
-
   self.postMessage(['s', 'Building gif...']);
-
   var encoder = new Whammy.Video(e.data[1]);
-
   for (var i = 0; i < data.length; i++) {
     encoder.add(data[i]);
   }
-
-  var url;
-
   self.postMessage(['s', 'Rendering...']);
   encoder.compile(false, function(output){
     self.postMessage(['s', 'Downloading...']);
     self.postMessage([output]);
     self.postMessage(['s', false]);
   });
-
-
-
 }, false);
